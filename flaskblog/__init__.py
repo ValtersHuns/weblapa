b502f4bed1f224cc60bec69de5a7ab6f2d24a6bd
ValtersHuns <60145218+ValtersHuns@users.noreply.github.com>
2020-03-24 16:46:22 +0200

    Update

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -5,7 +5,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://omlhmtowldtibl:919fc007ae69e2972fc17e2af30dd011d279b9dc079a90ec8fc9eff9a24b6f2a@ec2-54-247-79-178.eu-west-1.compute.amazonaws.com:5432/d2ui6905hi4ksl'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
--- flaskblog/models.py
+++ flaskblog/models.py
@@ -9,6 +9,7 @@ def load_user(user_id):
 
 
 class User(db.Model, UserMixin):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -21,6 +22,7 @@ class User(db.Model, UserMixin):
 
 
 class Post(db.Model):
+    __tablename__ = 'posts'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
