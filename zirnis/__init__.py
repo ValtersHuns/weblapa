8a4da78dc4bb13ff24a13bfc3dc73f2c42b4a489
ValtersHuns <60145218+ValtersHuns@users.noreply.github.com>
2020-03-25 20:07:07 +0200

    Datubāzes fix Nr.1

--- zirnis/__init__.py
+++ zirnis/__init__.py
@@ -5,7 +5,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://omlhmtowldtibl:919fc007ae69e2972fc17e2af30dd011d279b9dc079a90ec8fc9eff9a24b6f2a@ec2-54-247-79-178.eu-west-1.compute.amazonaws.com:5432/d2ui6905hi4ksl'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://kfrmjpcoyjtxkd:45c4e2d8b895c591400082c699f56f994223eec20ff5c9a4d4e16f77fab08702@ec2-54-75-231-215.eu-west-1.compute.amazonaws.com:5432/dtafemdinvcro'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
