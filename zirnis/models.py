57dc166e1712635ae740529225722b68cc905a11
ValtersHuns <60145218+ValtersHuns@users.noreply.github.com>
2020-04-02 15:15:40 +0300

    bik salaboj

--- zirnis/models.py
+++ zirnis/models.py
@@ -17,7 +17,7 @@ class User(db.Model, UserMixin):
     posts = db.relationship('Post', backref='author', lazy=True)
 
     def __repr__(self):
-        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+        return f"User('{self.username}', '{self.email}', '{self.image_file}' '{self.password}')"
 
 
 class Post(db.Model):
